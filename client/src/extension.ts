4446446bf0f6d7422c11cff2c8385e4542596eb8
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-03-06 12:50:25 -0800

    Scaffold language server + vscode client

---
+++ client/src/extension.ts
@@ -0,0 +1,72 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { workspace, ExtensionContext, window } from "vscode";
+
+import {
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  // The server is implemented in node
+  const traceOutputChannel = window.createOutputChannel(
+    "Athena Language Server trace"
+  );
+  const command = process.env.SERVER_PATH || "athena-language-server";
+  const run: Executable = {
+    command,
+    options: {
+      env: {
+        ...process.env,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        RUST_LOG: "debug",
+      },
+    },
+  };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [
+      { scheme: "file", language: "athena" },
+      { scheme: "file", language: "ath" },
+    ],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+    traceOutputChannel,
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "athena-language-server",
+    "Athena Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
---
+++ crates/athena-language-server/src/main.rs
@@ -0,0 +1,43 @@
+use tower_lsp::{
+    async_trait,
+    jsonrpc::Result,
+    lsp_types::{InitializeParams, InitializeResult, MessageType},
+    Client, LanguageServer, LspService, Server,
+};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+impl Backend {
+    fn new(client: Client) -> Self {
+        Self { client }
+    }
+}
+
+#[async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, _: tower_lsp::lsp_types::InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(Backend::new);
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
